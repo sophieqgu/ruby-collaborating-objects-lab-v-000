19d04cc358a7df0b5638b0d8912cd054cd56ea25
Sophie Gu <sophieqgu@gmail.com>
2019-03-22 00:49:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,8 @@ class Song
   def self.new_by_filename(filename)
     @name = filename.split(" - ")[1]
     @artist = filename.split(" - ")[0]
-    self.new(@name).artist = @artist 
-    self.new(@name)
+    song = self.new(@name)
+    song.artist = @artist 
+    song
   end 
 end 
