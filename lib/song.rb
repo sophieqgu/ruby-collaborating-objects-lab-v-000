c837d5512d5e4109e79bc752eeb34ddce15d5b0b
Sophie Gu <sophieqgu@gmail.com>
2019-03-22 00:46:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song 
+  attr_accessor :name, :artist
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def self.new_by_filename(filename)
+    @name = filename.split(" - ")[1]
+    @artist = filename.split(" - ")[0]
+    self.new(@name).artist 
+  end 
+end 
